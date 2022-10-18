a226cbb6f5fdecac39efb0a21344dbe75f2ce043
Koushik <55914356+koushik147@users.noreply.github.com>
2022-10-18 14:36:03 -0400

    Add files via upload

---
+++ Arrays1/Diagonal_Matrix.py
@@ -0,0 +1,37 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        
+        m = len(mat) # length of the rows
+        n = len(mat[0]) #length of the columns
+        result = [0]* (m*n) # Create an array with 0 for m*n
+        flag = 1    
+        index = 0
+        
+        r = 0
+        c = 0
+        
+        while index<len(result):    # Run until the length of result
+            result[index] = mat[r][c]   # Assign result[index] with matrix[rows][column]
+            
+            if flag == 1:   # if flag is 1
+                if c == n-1:    # if index is in the last column
+                    r+=1 # increment row by 1
+                    flag = -1 # Change the flag to -1
+                elif r == 0:    # if the index is in the fitst row
+                    c+=1 # increment column by 1
+                    flag = -1 # change the flag to -1
+                else:
+                    r-=1 # decrement row by 1
+                    c+=1 # increment column by 1
+            else:   
+                if r == m-1: # if index is in the last row
+                    c+=1 # increment column by 1
+                    flag = 1 # change the flag to 1
+                elif c == 0: # if the index is in the first column
+                    r+=1 # increment the row by 1
+                    flag = 1 # change the flag to 1
+                else:
+                    r+=1 # increment row by 1
+                    c-=1 # decrement the column by 1
+            index+=1 # increment the index by 1  
+        return result   # return result
---
+++ Arrays1/Spiral_Matrix.py
@@ -0,0 +1,37 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        
+        m = len(matrix) # lenght of rows
+        n = len(matrix[0]) # lenght of columns
+        
+        left = 0
+        right = n-1
+        top = 0
+        bottom = m-1
+        
+        result = []  # Create a empty array
+        
+        while left<=right and top<=bottom:  # Run until left less than right and top less than bottom
+            #Left to Right
+            for i in range(left, right+1):
+                result.append(matrix[top][i]) # Append the values in the result array as matrix[top][i]
+            top +=1 # increment top by 1
+            #Top to Bottom    
+            for j in range(top, bottom+1):  
+                result.append(matrix[j][right]) # Append the values in the result array as matrix[j][right]
+            right-=1
+            
+            if len(result) == m*n:  # if the length of result is equal to m*n
+                break
+            
+            #Right to Left
+            for k in range(right, left-1, -1):
+                result.append(matrix[bottom][k]) # Append the values in the result array as matrix[bottom][k]
+            bottom -=1
+                
+            #Bottom to Top
+            for l in range(bottom,top-1, -1):
+                result.append(matrix[l][left])  #Append the values in the result arry as matrix[l][left]
+            left+=1
+                
+        return result   # return result
